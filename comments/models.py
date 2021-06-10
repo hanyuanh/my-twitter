0e526e208c25478d3e4fbf8c5d3c8c78fc8b3cbd
hanyuanh <hanyuanhuang@hotmail.com>
2021-06-09 22:37:53 -0700

    Comments Model & Admin

---
+++ comments/models.py
@@ -0,0 +1,33 @@
+from django.contrib.auth.models import User
+from django.db import models
+from tweets.models import Tweet
+
+
+class Comment(models.Model):
+    """
+    In this version, a simple comment model is implemented, which is allowed
+    only on a certain tweet, isn't allowed on another comment.
+    """
+    user = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+    tweet = models.ForeignKey(Tweet, null=True, on_delete=models.SET_NULL)
+    content = models.TextField(max_length=140)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    user = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+    tweet = models.ForeignKey(Tweet, null=True, on_delete=models.SET_NULL)
+    content = models.TextField(max_length=140)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        # query of sorted-by-time comments under a certain tweet
+        index_together = (('tweet', 'created_at'),)
+
+    def __str__(self):
+        return '{} - {} says {} at tweet {}'.format(
+            self.created_at,
+            self.user,
+            self.content,
+            self.tweet_id,
+        )
--- testing/testcases.py
+++ testing/testcases.py
@@ -1,7 +1,8 @@
-from django.test import TestCase as DjangoTestCase  # to avoid name duplicate
+from comments.models import Comment
 from django.contrib.auth.models import User
-from tweets.models import Tweet
+from django.test import TestCase as DjangoTestCase  # to avoid name duplicate
 from rest_framework.test import APIClient
+from tweets.models import Tweet
 
 
 class TestCase(DjangoTestCase):
@@ -28,3 +29,8 @@ class TestCase(DjangoTestCase):
         if content is None:
             content = 'default tweet content'
         return Tweet.objects.create(user=user, content=content)
+
+    def create_comment(self, user, tweet, content=None):
+        if content is None:
+            content = 'default comment content'
+        return Comment.objects.create(user=user, tweet=tweet, content=content)
